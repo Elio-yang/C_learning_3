ee1a1511d617571ff0f955cd0f9ed531fbb24efb
ElioYang <2925539854@qq.com>
2020-01-16 14:41:50 +0800

    build up

---
+++ C_learning_3/complex_break_continue_area_rectangle.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+	float length, width;
+
+	printf("Enter the length of the rectangle(q to quit):\n");
+	while ((scanf("%f", &length)) == 1)
+	{
+		if (length <= 0) 
+		{
+            printf("No valid value were entered!\n");
+			printf("Please enter the length of the rectangle:\n");
+			continue;
+		}
+		printf("Length=%0.2f:\n",length);
+		printf("Enter its width(q to quit):\n");
+		while (scanf("%f", &width) == 1)
+	    {
+			if (width <= 0)
+			{
+				printf("No valid value were entered!\n");
+				printf("Please enter the width of the rectangle:\n");
+				continue;//back to the nearst while loop
+			}
+			else
+			{
+				printf("width=%0.2f\n", width);
+				break;//jump out this while loop 
+			}
+		}
+			
+		printf("Area=%0.2f\n", length*width);
+		printf("Enter the next length of the rectangle(q to quit):\n");
+	}
+	printf("Done");
+	return 0;
+}
